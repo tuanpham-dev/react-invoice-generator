0e875b12a2215cbcdc9a9030f585700c38156558
Tuan Pham <tuanpham.developer@gmail.com>
2020-07-02 16:14:27 -0400

    Updates

---
+++ precache-manifest.bf53781127de09809d251c73e47c0994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cc0fa3fa6e9aeddc6f16aabae5506d",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "708d12cc15cc4beb749b",
+    "url": "/react-invoice-generator/static/css/main.1ac1af28.chunk.css"
+  },
+  {
+    "revision": "702d9d2f40ed1bf0bdd2",
+    "url": "/react-invoice-generator/static/js/2.096bd69f.chunk.js"
+  },
+  {
+    "revision": "9bac9fa0fcb2b8097a6477e66dfc38d8",
+    "url": "/react-invoice-generator/static/js/2.096bd69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "708d12cc15cc4beb749b",
+    "url": "/react-invoice-generator/static/js/main.567bb14e.chunk.js"
+  },
+  {
+    "revision": "b905de3f642978c502d3",
+    "url": "/react-invoice-generator/static/js/runtime-main.2b3c76a5.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  }
+]);
