9248f6d27ec3ef5b2e53ac41711e79200e7f5c64
Tuan Pham <tuanpham.developer@gmail.com>
2020-11-02 11:14:25 -0500

    :sparkles: Add date picker into invoice date and invoice due date input

---
+++ src/components/EditableCalendarInput.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react'
+import { Text } from '@react-pdf/renderer'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import compose from '../styles/compose'
+
+interface Props {
+  className?: string
+  value?: string
+  selected?: Date
+  onChange?: (date: Date | [Date, Date] | null) => void
+  pdfMode?: boolean
+}
+
+const EditableCalendarInput: FC<Props> = ({ className, value, selected, onChange, pdfMode }) => {
+  return (
+    <>
+      {pdfMode ? (
+        <Text style={compose('span ' + (className ? className : ''))}>{value}</Text>
+      ) : (
+        <DatePicker
+          className={'input ' + (className ? className : '')}
+          selected={selected}
+          onChange={onChange ? (date) => onChange(date) : (date) => null}
+          dateFormat="MMM dd, yyyy"
+        />
+      )}
+    </>
+  )
+}
+
+export default EditableCalendarInput
